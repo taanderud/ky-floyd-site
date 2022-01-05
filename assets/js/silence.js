abec6bb02afc236b155a33f0bcaac7887d8a7476
Tricia Aanderud <taanderud@users.noreply.github.com>
2022-01-05 16:32:37 -0500

    updated

---
+++ assets/js/silence.js
@@ -0,0 +1,11 @@
+<!-- Silence -->
+<!--
+function fixem(id) {
+ var $one = "dakotabrookswrites";
+ var $bee = "gmail";
+ var $z = "com";
+ var
+  $hide = document.getElementById(id).value;
+ document.location.href = "mailto:" + $one + "@" + $bee + "." + $z + "?subject=" +
+  $hide;
+} -->
